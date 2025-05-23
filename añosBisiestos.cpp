b4cab7f63492ea9440ff1dde8cfc02a6f4e4f9ce
omarlnx <omarmorenoramir@gmail.com>
2025-05-23 10:20:58 -0600

    Implementa función de cálculo de años bisiestos

---
+++ añosBisiestos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+            int añoInicial;
+            int añoFinal;
+            int contador = 0;
+
+            cout<<"******PROGRAMA PARA CALCULAR AÑOS BISIESTOS EN UN PERIODO******"<<endl;
+            cout<<"Ingresa en año menor : ";
+            cin>>añoInicial;
+            cout<<"Ingresa en año mayor : ";
+            cin>>añoFinal;
+
+            for(int i =  añoInicial; i <= añoFinal; i++){
+                if((i%4==0 && i%100 != 0)||i%400==0){
+                   contador++;
+                }
+            }
+
+            cout<<"Total años bisiestos : "<<contador<<endl;
+
+
+    return 0;
+}
+
