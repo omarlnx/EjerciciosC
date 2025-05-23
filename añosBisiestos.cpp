0a1eeebf572b71bc29b32b0e22b5d6c4727cb0dd
omarlnx <omarmorenoramir@gmail.com>
2025-05-23 12:27:23 -0600

    Do While Implementado

--- añosBisiestos.cpp
+++ añosBisiestos.cpp
@@ -6,12 +6,32 @@ int main()
             int añoInicial;
             int añoFinal;
             int contador = 0;
-
             cout<<"******PROGRAMA PARA CALCULAR AÑOS BISIESTOS EN UN PERIODO******"<<endl;
+            do{
+
+
             cout<<"Ingresa en año menor : ";
             cin>>añoInicial;
             cout<<"Ingresa en año mayor : ";
             cin>>añoFinal;
+
+            if(añoInicial>añoFinal){
+                    cout<<"Ingrese primero el año menor despues el número mayor"<<endl;
+            }else{
+            for(int i =  añoInicial; i <= añoFinal; i++){
+                if((i%4==0 && i%100 != 0)||i%400==0){
+                   contador++;
+                }
+            }
+            }
+
+            }while(añoInicial>añoFinal);
+            cout<<"Total años bisiestos : "<<contador<<endl;
+
+
+
+
+/*
 
             for(int i =  añoInicial; i <= añoFinal; i++){
                 if((i%4==0 && i%100 != 0)||i%400==0){
@@ -20,7 +40,7 @@ int main()
             }
 
             cout<<"Total años bisiestos : "<<contador<<endl;
-
+*/
 
             //Modificar el codigo para implementar una estructura do-while o while
             //Si la fecha ingresada en mayor solicitar ingresar la fecha menor primero
